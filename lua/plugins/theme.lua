b702118f022b9112665cac299e8d5c0aa576394d
GodLight <godlightdsu@gmail.com>
2026-06-14 21:35:06 +0800

    update: 新增啟用透明背景的選項

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,15 @@
 -- 主題套件
-return { 
+return {
   "rebelot/kanagawa.nvim",
-  lazy = false, -- 啟動neovim時載入這個主題
+  -- "folke/tokyonight.nvim",
+  lazy = false,    -- 啟動neovim時載入這個主題
   priority = 1000, -- 確保這個主題在其他套件之前載入
   config = function()
+    -- require("kanagawa").setup({
+    --   transparent = true, -- 開啟透明背景
+    -- })
     -- 設定主題
     vim.cmd.colorscheme("kanagawa")
+    -- vim.cmd.colorscheme("tokyonight-moon") -- 使用 moon 變體
   end,
 }
